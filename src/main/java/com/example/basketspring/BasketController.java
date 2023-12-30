24d587c5d74d1382c027a37936a60be054adbddf
Cl1yMba <petrmanastryly199@gmail.com>
2023-12-30 13:22:40 +0200

    Initial Commit!

---
+++ src/main/java/com/example/basketspring/BasketController.java
@@ -0,0 +1,26 @@
+package com.example.basketspring;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class BasketController {
+    private BasketService basketService;
+    public BasketController(BasketService basketService) {
+        this.basketService = basketService;
+    }
+
+
+    @GetMapping("/add")
+    public String addToBasket(@RequestParam("id") List<Long> ids) {
+        basketService.add(ids);
+        return "Items added successfully";
+    }
+
+    @GetMapping("/get")
+    public List<Long> getBasket() {
+        return basketService.get();
+    }
+}
+
---
+++ src/main/java/com/example/basketspring/BasketService.java
@@ -0,0 +1,22 @@
+package com.example.basketspring;
+import org.springframework.stereotype.Service;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BasketService {
+    private final Basket basket;
+
+    public BasketService(Basket basket){
+        this.basket = basket;
+    }
+
+    public void add(List<Long> itemIds) {
+       basket.addItem(itemIds);
+    }
+
+    public List<Long> get() {
+       return basket.getItems();
+    }
+}
